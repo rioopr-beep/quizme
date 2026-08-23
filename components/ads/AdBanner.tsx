2d0e836482613c8d24345c788102886d085e3fdd
rioopr-beep <riokmxl@gmail.com>
2026-08-23 13:42:09 +0700

    Update AdBanner.tsx

--- components/ads/AdBanner.tsx
+++ components/ads/AdBanner.tsx
@@ -1,6 +1,6 @@
-'use client'
+'use client';
 
-import Script from 'next/script'
+import Script from 'next/script';
 
 export default function AdBanner() {
   return (
@@ -11,5 +11,5 @@ export default function AdBanner() {
         __html: `(function(s){s.dataset.zone='11635566',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`,
       }}
     />
-  )
+  );
 }

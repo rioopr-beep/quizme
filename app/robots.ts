528750be1066929887ec8ec9a56b1a08ad7533f6
rioopr-beep <riokmxl@gmail.com>
2026-08-27 11:44:30 +0700

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -4,11 +4,13 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = 'https://www.quizfrend.my.id';
 
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/admin', '/dashboard', '/school', '/quiz'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/', '/quiz/*/preview'],
+        disallow: ['/admin', '/dashboard', '/school', '/quiz'],
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }

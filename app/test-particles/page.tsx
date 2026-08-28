9a30ffc8897186b83c1fdda0b442fc0e288ce0b3
rioopr-beep <riokmxl@gmail.com>
2026-08-28 09:46:49 +0700

    Create page.tsx

---
+++ app/test-particles/page.tsx
@@ -0,0 +1,9 @@
+import AuthParticleField from '../../components/AuthParticleField';
+
+export default function TestPage() {
+  return (
+    <main className="relative min-h-screen bg-slate-50">
+      <AuthParticleField />
+    </main>
+  );
+}
